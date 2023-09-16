17435431d71fc89067c2080f53b4bbeb8cfc7826
LillyMee <89249209+LillyMee@users.noreply.github.com>
2023-09-16 20:30:55 +0200

    Add files via upload

---
+++ src/fa2_cardelearship/FA2_CarDelearship.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fa2_cardelearship;
+
+/**
+ *
+ * @author ldhli
+ */
+public class FA2_CarDelearship {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
